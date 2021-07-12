2b93fb13d4da24ec05573aa47da423e142739d09
Arjunsingh2 <53118674+Arjunsingh2@users.noreply.github.com>
2021-07-13 03:33:49 +0530

    Add files via upload

---
+++ Repositories/ITreatmentRepo.cs
@@ -0,0 +1,15 @@
+﻿using IPTreatmentMicroservice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPTreatmentMicroservice.Repositories
+{
+    public interface ITreatmentRepo
+    {
+        // public IEnumerable<TreatmentPlan> GetTreatmentPlan(string ailment,string packageName);
+        //public IEnumerable<TreatmentPlan> GetTreatmentPlan(string ailment);
+        public void Addtodb(TreatmentPlan plan);
+    }
+}
---
+++ Repositories/TreatmentRepo.cs
@@ -0,0 +1,35 @@
+﻿using IPTreatmentMicroservice.Models;
+//using IPTreatmentMicroservice.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPTreatmentMicroservice.Repositories
+{
+    public class TreatmentRepo : ITreatmentRepo
+    {
+        private static TreatmentDBContext _context;
+
+        public TreatmentRepo()
+        {
+        }
+
+        public TreatmentRepo(TreatmentDBContext context)
+        {
+            _context = context;
+        }   
+        public void Addtodb(TreatmentPlan plan)
+        {
+            _context.Add(plan);
+            _context.SaveChanges();
+        }
+            
+   
+        
+        //public IEnumerable<TreatmentPlan> GetTreatmentPlan(string ailment)
+        //{
+        //    return _context.TreatmentPlans.Where(t => t.Ailment == ailment );
+        //}
+    }
+}
